ba1f5b11062e6390d664d2187b0c01cd94ea18ef
Vince Andrew Pimentel <contact@vincep.me>
2018-10-23 00:09:34 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,7 +19,12 @@ class Song
   def self.new_by_name(name)
     song = self.new
     song.name = name
-    song.save
+    # song.save
     song
   end
+
+  # def self.create_by_name(name)
+  #   song = self.new
+  #
+  # end
 end
